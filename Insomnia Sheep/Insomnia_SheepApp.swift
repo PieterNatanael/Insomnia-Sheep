aa490c025c9ffd9c941c97e209b2d08a32e0d303
PieterNatanael <pieternatanael@icloud.com>
2024-03-20 20:13:31 +0800

    Initial Commit

---
+++ Insomnia Sheep/Insomnia_SheepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insomnia_SheepApp.swift
+//  Insomnia Sheep
+//
+//  Created by Pieter Yoshua Natanael on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Insomnia_SheepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
